50d54982870b9786d9b4178112f5574641f4573e
Altaf Quadri <altafmquadri@gmail.com>
2020-04-01 00:28:51 -0400

    add ball paddle score

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
 const rulesBtn = document.getElementById('rules-btn')
 const closeBtn = document.getElementById('close-btn')
 const rules = document.getElementById('rules')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+let score = 0
 
+
+
+//create ball
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+}
+
+//create paddle
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0
+}
+
+//draw ball on canvas
+const drawBall = () => {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+//draw paddle
+const drawPaddle = () => {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+const draw = () => {
+    drawBall()
+    drawPaddle()
+    drawScore()
+}
+
+const drawScore = () => {
+    ctx.font = '20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+}
 
 //rules and close event handler
 rulesBtn.addEventListener('click', () => {
@@ -10,3 +63,6 @@ rulesBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     rules.classList.remove('show')
 })
+
+
+draw()
